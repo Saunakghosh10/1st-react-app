39afb39861bd941b2db8e3ee8f9cc5c2441904da
Saunak <76943154+Saunakghosh10@users.noreply.github.com>
2022-04-06 14:56:10 +0530

    2nd commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+// eslint-disable-next-line no-unused-vars
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import Counter from './components/counter';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ <Counter/>,
   document.getElementById('root')
 );
 
